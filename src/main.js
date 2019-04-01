4ca1b504f2d16c7dfd7f77941a5d9691bb992f85
Wohlig <ram.chaudhari@wohlig.com>
2019-04-01 18:41:08 +0530

    navbar created

--- src/main.js
+++ src/main.js
@@ -2,7 +2,87 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import {
+  faCoffee,
+  faShieldAlt,
+  faQuoteLeft,
+  faFingerprint,
+  faCaretRight,
+  faLaptop,
+  faChevronRight,
+  faBars,
+  faSquare,
+  faCalendar,
+  faCircle,
+  faPencilAlt,
+  faUserSecret,
+  faCaretDown,
+  faTh,
+  faDumbbell,
+  faEnvelopeOpenText,
+  faUser
+} from "@fortawesome/free-solid-svg-icons"; //fas fa
+
+import {
+  faLinkedinIn,
+  faFacebookSquare,
+  faTwitter,
+  faGoogle,
+  faYoutube,
+  faInstagram,
+  faDribbble,
+  faApple
+} from "@fortawesome/free-brands-svg-icons"; //fab
+
+import { faCommentAlt } from "@fortawesome/free-regular-svg-icons"; //far
+
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(
+  faCircle,
+  faPencilAlt,
+  faBars,
+  faCalendar,
+  faSquare,
+  faQuoteLeft,
+  faChevronRight,
+  faLaptop,
+  faCoffee,
+  faShieldAlt,
+  faFingerprint,
+  faCaretRight,
+  faLaptop,
+  faUserSecret,
+  faCaretDown,
+  faTh,
+  faDumbbell,
+  faEnvelopeOpenText,
+  faUser
+); //fas
+
+library.add(faCommentAlt); //far
+
+library.add(
+  faLinkedinIn,
+  faFacebookSquare,
+  faTwitter,
+  faGoogle,
+  faYoutube,
+  faInstagram,
+  faDribbble,
+  faApple
+); //fab
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
 new Vue({
---
+++ src/views/Moviepage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Navbar></Navbar>
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+export default {
+  name: "NavBar",
+  components:{
+      Navbar
+  }
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
