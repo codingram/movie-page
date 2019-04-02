ceaeb8a7d37ef1986ecd332d04f0b65c78003293
Ram Chaudhari <ram.chaudhari@wohlig.com>
2019-04-02 08:08:42 +0530

    same

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import BootstrapVue from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
-
 import { library } from "@fortawesome/fontawesome-svg-core";
 
 import {
